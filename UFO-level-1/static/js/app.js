2ee8ca7f17d62b63e1fb31ca134ae05f86a82862
charles youngman <charlesyoungman@charless-MacBook-Air.local>
2021-06-29 00:08:57 -0400

    adding new files

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -14,27 +14,50 @@ data.forEach((ufoData) => {
     });
   });
   
-var button = d3.select(".btn");
+var button = d3.select("#filter-btn");
 
-var form = d3.select("#form");
+// var form = d3.select("#form");
 
 button.on("click", runEnter);
-form.on("submit",runEnter);
+// form.on("submit",runEnter);
 
 function runEnter() {
 
     d3.event.preventDefault();
 
-    var inputElement = d3.select(".form-control");
+    var inputElement = d3.select("#datetime");
 
     var inputValue = inputElement.property("value");
 
     console.log(inputValue);
-    console.log(tableData);
+  
 
     var filteredData = tableData.filter(ufo => ufo.datetime === inputValue);
 
     console.log(filteredData);
+    tbody.html("")
+    filteredData.forEach((ufoData) => {
+      var row = tbody.append("tr");
+      Object.entries(ufoData).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+      });
+    });
+
+
+    $(button).click(function() {
+      $(inputElement).val('');
+      table.search('').draw(); 
+  });
+
+// var date = filteredData.map(time => time.data);
+
+// var list = d3.select(".list-group");
+
+// list.html("");
+
+// list.append("li").text("datetime");
+// list.append("li").text("city")
 
 };
 

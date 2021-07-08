ca34a18595e94a7b25b75fdf0ce12565fd2c7e25
charles youngman <charlesyoungman@charless-MacBook-Air.local>
2021-07-08 19:00:03 -0400

    adding new files

---
+++ UFO-level-2/static/js/app.js
@@ -0,0 +1,92 @@
+// from data.js
+var tableData = data;
+
+// YOUR CODE HERE!
+var tbody = d3.select("tbody");
+
+console.log(tableData)
+
+data.forEach((ufoData) => {
+    var row = tbody.append("tr");
+    Object.entries(ufoData).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+  
+var button = d3.select("#filter-btn");
+
+// var form = d3.select("#form");
+
+button.on("click", runEnter);
+// form.on("submit",runEnter);
+
+function runEnter() {
+
+    d3.event.preventDefault();
+
+    var inputElement = d3.select("#datetime");
+
+    var inputValue = inputElement.property("value");
+
+    console.log(inputValue);
+  
+
+    var filteredData = tableData.filter(ufo => ufo.datetime === inputValue);
+
+    console.log(filteredData);
+    tbody.html("")
+    filteredData.forEach((ufoData) => {
+      var row = tbody.append("tr");
+      Object.entries(ufoData).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+      });
+    });
+  };
+
+    $(button).click(function() {
+      $(inputElement).val('');
+      table.search('').draw(); 
+  });
+
+// var date = filteredData.map(time => time.data);
+
+// var list = d3.select(".list-group");
+
+// list.html("");
+
+// list.append("li").text("datetime");
+// list.append("li").text("city")
+
+// };
+
+// inputValue.on("change", function() {
+//   var newText = d3.event.target.value;
+//   console.log(newText);
+// });
+
+// function runEnter() {
+
+//   d3.event.preventDefault();
+
+//   var inputElement = d3.select("#city");
+
+//   var inputValue = inputElement.property("value");
+
+//   console.log(inputValue);
+
+// var filteredData1 = tableData.filter(ufo => ufo.city === inputValue);
+
+//     console.log(filteredData1);
+//     tbody.html("")
+//     filteredData.forEach((ufoData) => {
+//       var row = tbody.append("tr");
+//       Object.entries(ufoData).forEach(([key, value]) => {
+//         var cell = row.append("td");
+//         cell.text(value);
+//       });
+//     });
+//   };
+
+
